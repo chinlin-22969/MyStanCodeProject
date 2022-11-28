dddc71db1c0f4b38f754ed3c14bc716a44992745
lynnxgoinfo <119319990+lynnxgoinfo@users.noreply.github.com>
2022-11-28 21:35:55 +0800

    Add files via upload

---
+++ stanCode_Projects/anagram_searcher/anagram.py
@@ -0,0 +1,126 @@
+"""
+File: anagram.py
+Name:
+----------------------------------
+This program recursively finds all the anagram(s)
+for the word input by user and terminates when the
+input string matches the EXIT constant defined
+at line 19
+
+If you correctly implement this program, you should see the
+number of anagrams for each word listed below:
+    * arm -> 3 anagrams
+    * contains -> 5 anagrams
+    * stop -> 6 anagrams
+    * tesla -> 10 anagrams
+    * spear -> 12 anagrams
+"""
+
+import time                   # This file allows you to calculate the speed of your algorithm
+
+# Constants
+FILE = 'dictionary.txt'       # This is the filename of an English dictionary
+EXIT = '-1'                   # Controls when to stop the loop
+
+
+def main():
+    """
+    TODO: Find all the anagrams of the input word!
+    """
+    start = time.time()
+    ####################
+    print('Welcome to stanCode "Anagram Generator" (or -1 to quit)')
+    s = input('Find anagrams for: ')
+    find_anagrams(s)
+    ####################
+    end = time.time()
+    print('----------------------------------')
+    print(f'The speed of your anagram algorithm: {end-start} seconds.')
+
+
+def read_dictionary(s):
+    """
+    :param s:(str) the input word from user
+    :return words_lst:(list) A list of words from the FILE having the same length as s
+    and containing all the characters of s
+    """
+    words_lst = []
+    with open(FILE) as f:
+        for line in f:
+            word = line.strip()
+            if len(word) == len(s):  # find out words which it's length equal to s's
+                if sorted(word) == sorted(s):  # find out words which contains all the character in s
+                    words_lst.append(line.strip())
+    return words_lst
+
+
+def has_prefix_helper(sub_s, words_lst):
+    """
+    :param sub_s:(str) part of the input words
+    :param words_lst:(list) A list of given words
+    :return Boolean
+    """
+    for word in words_lst:
+        if word.startswith(sub_s):
+            return True
+    return False
+
+
+def find_anagrams(s):
+    """
+    :param s:(str) The input word from user
+    This function returns nothing.
+    """
+    s_lst = []
+    for i in range(len(s)):
+        s_lst.append((i, s[i]))
+    ans_lst = find_anagrams_helper(s, s_lst, [], '', [], read_dictionary(s), len(s))
+    print(f'{len(ans_lst)} anagrams: {ans_lst}')
+
+
+def index_exist(index, cur_lst):
+    """
+    :param index: (int) the index of the character
+    :param cur_lst: (list) A list of tuples containing index and character
+    :return Boolean
+    """
+    for t in cur_lst:
+        if index == t[0]:
+            return True
+    return False
+
+
+def find_anagrams_helper(s, s_lst, cur_lst, cur_str, ans_lst, words_lst, ans_len):
+    """
+    :param s: (str)The input word from user /ex: arm
+    :param s_lst: (list) A list of tuples containing index and character of s /ex:[(1,'a'), (2,'r'), (3,'m')]
+    :param cur_lst: (list) A list holding current tuples containing index and character/ ex:[(3,'m'), (1,'a')]
+    :param cur_str: (str) holding the current string/ ex:'ma'
+    :param ans_lst: (list) A list holding anagrams of s
+    :param words_lst: (list) A list of words having the same length as s from the FILE
+    :param ans_len: (int) The length of s
+
+    :return ans_lst: (list) A list holding anagrams of s
+    """
+    if cur_str in read_dictionary(s):
+        if cur_str not in ans_lst:
+            print('Searching...')
+            print(f'Found: {cur_str}')
+            ans_lst.append(cur_str)
+    else:
+        for t in s_lst:
+            if not index_exist(t[0], cur_lst):
+                # Choose
+                cur_lst.append(t)
+                cur_str += t[1]
+                # Explore
+                if has_prefix_helper(cur_str, words_lst):
+                    find_anagrams_helper(s, s_lst, cur_lst, cur_str, ans_lst, words_lst, ans_len)
+                # Un-choose
+                cur_lst.pop()
+                cur_str = cur_str[:len(cur_str) - 1]
+    return ans_lst
+
+
+if __name__ == '__main__':
+    main()
---
+++ stanCode_Projects/boggle_game_solver/boggle.py
@@ -0,0 +1,134 @@
+"""
+File: boggle.py
+Name:
+----------------------------------------
+TODO:
+"""
+
+import time
+
+# This is the file name of the dictionary txt file
+# we will be checking if a word exists by searching through it
+FILE = 'dictionary.txt'
+
+
+def main():
+	"""
+	TODO: This program will present the game "boggle".
+	"""
+	start = time.time()
+	####################
+	ch_dict = {}
+	s_str = ''  # ghjktyghffffbbbb
+	# Ask the user to input four rows of letter
+	for i in range(4):
+		s = input(f'{i+1} row of letters: ')
+		# Program stops if illegal input happens
+		if len(s) != 7 or s[1] + s[3] + s[5] != '   ':
+			print('Illegal input')
+			exit()
+
+	# Turn all the input into a string
+		sub_str = ''.join(s.split())
+	# Turn all the input into a dictionary (key: value --> column: row)
+		for j in range(4):
+			ch_dict[j, i] = sub_str[j]
+		s_str += sub_str
+
+	# Word_dict contains all the words start with letters from s_str (key: value --> 1st letter: words)
+	word_dict = read_dictionary(s_str)
+
+	# Check out the answer of each input letter one by one
+	ans_lst = []
+	for i in range(4):
+		for j in range(4):
+			boggle(word_dict, [(j, i)], ch_dict, ch_dict[(j, i)], j, i, ans_lst)
+	print(f'There are {len(ans_lst)} words in total.')
+
+	####################
+	end = time.time()
+	print('----------------------------------')
+	print(f'The speed of your boggle algorithm: {end - start} seconds.')
+
+
+def read_dictionary(s):
+	"""
+	This function reads file "dictionary.txt" stored in FILE
+	and find out the words which starts with the letter in s
+	and make it into a dictionary.(key: value --> letter in s: words start with the letter in s)
+
+	:param s: A string contains the input letters
+	:return word_dict: A dictionary contains words which starts with character in s. {'a':['apple',..],'b':['booth'..].}
+	"""
+	word_dict = {}
+	with open(FILE) as f:
+		for line in f:
+			word = line.strip()
+			if word[0] in s:
+				if word[0] in word_dict:
+					word_dict[word[0]].append(word)
+				else:
+					word_dict[word[0]] = [word]
+	return word_dict
+
+
+def has_prefix(sub_s, word_dict):
+	"""
+	This function check if there is any words in word_dict with prefix 'sub_s'.
+
+	:param sub_s: (str) A substring that is constructed by neighboring letters on a 4x4 square grid
+	:param word_dict: A dictionary contains all the words which starts with the letter in user inputs.
+	ex:{'a':['apple',..], 'b':['booth'...].....}
+
+	:return: (bool) If there is any words with prefix stored in sub_s
+	"""
+	for word in word_dict[sub_s[0]]:
+		if word.startswith(sub_s):
+			return True
+	return False
+
+
+def boggle(word_dict, index_lst, ch_dict, cur_str, cur_x, cur_y, ans_lst):
+	"""
+	This function concatenate the surrounding letters to cur_str and check if it fix has_prefix.
+	Will print every words that fix the game 'boggle' and store the words in the ans_lst.
+
+	:param word_dict: A dictionary contains all the words which starts with the letter in user inputs
+	:param index_lst: A list contains the x, y location of the input letters
+	:param ch_dict: A dictionary key:value -->  location: input letter  ex:{(0, 0):'f', (1, 0):'y'.....}
+	:param cur_str: current string starts with an input letter
+	:param cur_x: current x location(column)
+	:param cur_y: current y location(row)
+	:param ans_lst: A list contains the answer string
+
+	This function do not return.
+	"""
+	# Base-case
+	if len(cur_str) >= 4:
+		if cur_str not in ans_lst:
+			if cur_str in word_dict[cur_str[0]]:
+				print(f'Found: "{cur_str}"')
+				ans_lst.append(cur_str)
+
+	# Find the surrounding letters, and check if it fits has_prefix
+	for y in range(-1, 2):
+		for x in range(-1, 2):
+			# Choose
+			if (cur_x+x, cur_y+y) not in index_lst:
+				if 0 <= cur_x+x < 4 and 0 <= cur_y+y < 4:
+					cur_x += x
+					cur_y += y
+					cur_str += ch_dict[(cur_x, cur_y)]
+					index_lst.append((cur_x, cur_y))
+					# Explore
+					if has_prefix(cur_str, word_dict):
+						boggle(word_dict, index_lst, ch_dict, cur_str, cur_x, cur_y, ans_lst)
+					# Un-choose
+					cur_str = cur_str[:-1]
+					index_lst.pop()
+					cur_x -= x
+					cur_y -= y
+
+
+if __name__ == '__main__':
+	main()
